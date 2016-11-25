2e207a98be22faed7e236e59858115bba87bba23
Tom LeConey <tleconey@gmail.com>
2016-11-25 11:14:03 -0600

    added navigation between post index page and post new page using Link component from react-router

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 // action creator we want to call inside componentWillMount
 import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsIndex extends Component {
 	// lifecycle method that React automatically calls once component is about to be rendered to DOM for first time
@@ -11,7 +12,14 @@ class PostsIndex extends Component {
 
 	render() {
 		return (
-		  <div> List of Blog Posts </div>
+		  <div> 
+		  	<div className="text-xs-right">
+		  		<Link to="/posts/new" className="btn btn-primary">
+		  			Add a post
+		  		</Link>
+		  	</div>
+		  	List of Blog Posts 
+		  </div>
 		 );
 	}
 }
