bd5bb6132609edee4d18675286db0eda1852cac1
Tom LeConey <tleconey@gmail.com>
2016-11-24 20:57:12 -0600

    created test component to make sure app is working as intended

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
+// >> file where we house all the routing between paths and a given component we want to show
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
 
+const Greeting = () => {
+	return <div> Hey there! </div>;
+};
+
 export default (
 	// matches this component to this endpoint aka path
-	<Route path="/" component={App} />
+	<Route path="/" component={App} >
+	  <Route path="greet" component={Greeting} />
+	  <Route path="greet2" component={Greeting} />
+	  <Route path="greet3" component={Greeting} />
+	</Route>
 );
