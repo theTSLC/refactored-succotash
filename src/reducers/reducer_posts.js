a712073333864b99c492869fb03180ead3953bc7
Tom LeConey <tleconey@gmail.com>
2016-11-24 22:07:10 -0600

    fixed error from typos

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,4 +1,4 @@
-import { FETCH_POSTS } from './actions/index';
+import { FETCH_POSTS } from '../actions/index';
 
 const INITIAL_STATE = { all: [], post: null };
 
@@ -6,7 +6,7 @@ const INITIAL_STATE = { all: [], post: null };
 export default function(state = INITIAL_STATE, action) {
 	switch(action.type) {
 		case FETCH_POSTS:
-
+		  return { ...state, all: action.payload.data };
 		default:
 			return state;
 	}
