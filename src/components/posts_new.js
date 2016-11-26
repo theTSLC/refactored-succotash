c36ff877c27af1d51201e83582237daee28901ec
Tom LeConey <tleconey@gmail.com>
2016-11-26 13:14:22 -0600

    added cancel button to return to index & error messages to display red for form validation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsNew extends Component {
  render() {
@@ -14,32 +15,33 @@ class PostsNew extends Component {
  	return(
  		<form onSubmit={handleSubmit(this.props.createPost)}>
  		  <h3> Create A New Post </h3>
- 		  <div className="form-group">
+
+ 		  <div className={`form-group ${title.touched && title.invalid ? "has-danger" : ''}`}>
  		  	<label>Title</label>
  		  	<input type="text" className="form-control" {...title} />
- 		  	<div className="text-help">
+ 		  	<div className="form-control-label">
  		  	  {title.touched ? title.error : ''}
  		  	 </div>
  		  </div>
 
- 		  <div className="form-group">
+ 		  <div className={`form-group ${categories.touched && categories.invalid ? "has-danger" : ''}`}>
  		  	<label> Categories </label>
  		  	<input type="text" className="form-control" {...categories} />
- 		  	<div className="text-help">
+ 		  	<div className="form-control-label">
  		  	  {categories.touched ? categories.error : ''}
  		  	 </div>
  		  </div>
 
- 		  <div className="form-group">
+ 		  <div className={`form-group ${content.touched && content.invalid ? "has-danger" : ''}`}>
  		  	<label> Content </label>
  		  	<textarea className="form-control" {...content} />
- 		  	 		  	<div className="text-help">
+ 		  	 		  	<div className="form-control-label">
  		  	  {content.touched ? content.error : ''}
  		  	 </div>
  		  </div>
 
  		  <button type="submit" className="btn btn-primary"> Submit </button>
-
+ 		  <Link to="/" className="btn btn-danger"> Cancel </Link>
  		</form>
  	);
  }
