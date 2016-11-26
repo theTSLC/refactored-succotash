9316656a34a2915ff1a7a31b738702399786522e
Tom LeConey <tleconey@gmail.com>
2016-11-26 14:50:31 -0600

    succesfully linking each post from post index to that individual post

--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,14 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
+
 
 export default (
 	// matches this component to this endpoint aka path
 	<Route path="/" component={App} >
 	  <IndexRoute component={PostsIndex} />
 	  <Route path="posts/new" component={PostsNew} />
+	  <Route path="posts/:id" component={PostsShow} />
 	</Route>
 );
