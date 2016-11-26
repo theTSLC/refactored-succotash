051cebd387a2f7b13e874e18d38ec27479d9fa02
Tom LeConey <tleconey@gmail.com>
2016-11-25 20:41:10 -0600

    added field information for redux-forms and created form layout in JSX

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import PostsReducer from './reducer_posts';
-
+// imports reducer and creates variable of it named formReducer
+import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
-	posts: PostsReducer
+	posts: PostsReducer,
+	form: formReducer
 });
 
 export default rootReducer;
