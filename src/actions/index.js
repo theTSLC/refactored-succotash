c918f5fdc0100205dab81e43a98b066fabcf6c60
Tom LeConey <tleconey@gmail.com>
2016-11-25 21:16:10 -0600

    200 status on posting new post, after finishing hook up in posts_new component

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
 
-const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const ROOT_URL = 'https://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=puss&boots';
 
 export function fetchPosts() {
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
  render() {
@@ -11,7 +12,7 @@ class PostsNew extends Component {
  	const {fields: {title, categories, content}} = this.props;
 
  	return(
- 		<form onSubmit={handleSubmit}>
+ 		<form onSubmit={handleSubmit(this.props.createPost)}>
  		  <h3> Create A New Post </h3>
  		  <div className="form-group">
  		  	<label>Title</label>
@@ -35,7 +36,10 @@ class PostsNew extends Component {
  }
 }
 
+//conenct: 1st argument is mapStateToProps, 2nd is mapDispatchToProps
+//reduxForm: 1st argument is form confid, 2nd is mapStateToProps, 3rd is mapDispatchToProps
+
 export default reduxForm({
 	form: 'PostsNewForm',
 	fields: ['title', 'categories', 'content']
-})(PostsNew);
+}, null, {createPost})(PostsNew);
